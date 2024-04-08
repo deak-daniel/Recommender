da4b8d2920d8222e9be73a0b298507ebc176cde3
Deak Daniel <83765724+deak-daniel@users.noreply.github.com>
2024-04-08 18:46:56 +0200

    Add project files.

---
+++ Recommender/Recommender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recommender
+{
+    internal class Recommender
+    {
+        #region Public properties
+        public List<string> Data { get; private set; }
+
+        #endregion // Public properties
+
+        #region Constructor
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="inputfile">Input data file.</param>
+        public Recommender(string inputfile)
+        {
+            Data = new List<string>();
+            ExtractData(inputfile);
+        }
+        #endregion // Constructor
+
+        #region Private methods
+        /// <summary>
+        /// This method extracts certain data from the input file (it is hardcoded, and can only be used with one data source).
+        /// </summary>
+        /// <param name="inputfile">Input file path.</param>
+        private void ExtractData(string inputfile)
+        {
+            foreach (string item in File.ReadAllLines(inputfile))
+            {
+                string[] helper = item.Split(",");
+                Data.Add($"{helper[0]} {helper[1]} {helper[2]} {helper[4]}");
+            }
+        }
+        #endregion
+    }
+}
